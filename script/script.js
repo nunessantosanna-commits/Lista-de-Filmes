1e95a1b298319a3df5d93a8e1499c183ce628e45
Anna Nunes <nunes.santos.anna@escola.pr.gov.br>
2026-06-02 10:40:39 -0300

    ok

--- script/script.js
+++ script/script.js
@@ -1,37 +1,37 @@
 const filmes = [
    {
     título:"Vingadores",
-    Imagem:"https://www.themoviedb.org/movie/299534-avengers-endgame",
+    Imagem:"https://media.themoviedb.org/t/p/w600_and_h900_face/9fRX8UKlIW7Lb9GqNsJVakWWFCi.jpg",
     descrição:"Ultimato",
 
    },
     { 
         título:"Off campus:Amores improváveis",
-        Imagem:"https://www.themoviedb.org/movie/299534-avengers-endgame",
+        Imagem:"https://media.themoviedb.org/t/p/w600_and_h900_face/iQ1WbmoC08ql4qRZkfUD2LlzNOL.jpg",
         descrição:"Drama"
     
    },
     {
         título:"Por cima do seu cadáver",
-        imagem:"https://www.themoviedb.org/movie/1390300-over-your-dead-body",
+        imagem:"https://media.themoviedb.org/t/p/w600_and_h900_face/9ghTSDRXDYZagU9hOf285EjOmgD.jpg",
         descrição:"Terror",
         },
 
         {
             título:"Como mágica",
-            imagem:"https://www.themoviedb.org/movie/1007757-swapped",
+            imagem:"https://media.themoviedb.org/t/p/w600_and_h900_face/7FBnCkYzHlVHEtuzTEiGgFhs8EQ.jpg",
             descrição:"Fantasia",
         },
 
         {
             título:"Trocando as bolas",
-            imagem:"https://www.themoviedb.org/movie/1621-trading-places?language=pt-BR",
+            imagem:"https://media.themoviedb.org/t/p/w600_and_h900_face/tYyHmUzo6GT7qjG7OWqo0zsXcoJ.jpg",
             descrição:"Comédia",
         },
 
         {
         título:"Spider-Noir",
-        imagem:"https://www.themoviedb.org/tv/220102-spider-noir?language=pt-BR",
+        imagem:"https://media.themoviedb.org/t/p/w600_and_h900_face/rfrzXSaiuvRMLo8PRQds0TfFrZC.jpg",
         descrição:"Drama e Mistério",
         }
 
@@ -47,7 +47,7 @@ function mostrarFilmes(lista) {
                     <img src="${filme.imagem}" class="card-img-top">
                     <div class="card-body">
                         <h3>${filme.titulo}</h3>
-                        <p>${filme.descricao}</p>
+                        <p>${filme.descrição}</p>
                         <button class="btn btn-dark" onclick="verDetalhes('${filme.titulo}')">Ver detalhes"</button>
                     </div>
                 </div>
