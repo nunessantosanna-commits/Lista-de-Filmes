9b4901b5068c05cb11d74cf5fe394f2e97ac775e
Anna Nunes <nunes.santos.anna@escola.pr.gov.br>
2026-06-08 10:55:17 -0300

    ok

--- script/dorama.js
+++ script/dorama.js
@@ -1,52 +1,54 @@
-const filmes = [
+const dorama = [
     {
-        título:"Vingadores: Ultimato",
-        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/9fRX8UKlIW7Lb9GqNsJVakWWFCi.jpg",
-        descrição: "Após os eventos devastadores de Vingadores: Guerra Infinita, o universo está em ruínas devido aos esforços do Titã Louco, Thanos. Com a ajuda de aliados remanescentes, os Vingadores devem se reunir mais uma vez a fim de desfazer as ações de Thanos e restaurar a ordem no universo de uma vez por todas, não importando as consequências.",
+        título:"Amor e batatas",
+        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/jLsYpsmWuYunFWbje041flWpnGM.jpg",
+        descrição: "A vida de uma pesquisadora de batatas vira de cabeça para baixo quando um novo diretor chega ao laboratório, provando que o amor pode brotar nos lugares mais inesperados.",
 
     },
     {
-        título: "10 coisas que eu odeio em você",
-        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/lRy6impzDHOqTX3A2T5dxI6Lwfu.jpg",
-        descrição: "Em seu primeiro dia na nova escola, Cameron se apaixona por Bianca. Mas ela só poderá sair com rapazes até que Kat, sua irmã mais velha, arrume um namorado. O problema é que ela é insuportável. Cameron, então, negocia com o único garoto que talvez consiga sair com Kat – um misterioso bad-boy com uma péssima reputação."
+        título: "Soriso Real",
+        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/bvUvdwfeDBlyb9sg85FulzGZs0P.jpg",
+        descrição: "Em meio a uma disputa pela herança, um charmoso herdeiro entra em conflito com uma funcionária e seu enorme sorriso, que ele simplesmente não suporta.",
 
     },
     {
-        título: "Por cima do seu cadáver",
-        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/9ghTSDRXDYZagU9hOf285EjOmgD.jpg",
-        descrição: "Uma comédia de humor ácido com um toque de suspense, dirigida por Jorma Taccone e estrelada por um casal disfuncional, Dan e Lisa, que se refugiam em uma cabana isolada com o objetivo secreto de se assassinarem. Seus elaborados planos para se matarem são interrompidos quando foragidos e um agente penitenciário corrupto atrapalham sua fuga.",
+        título: "Beleza Verdadeira",
+        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/g7ocbs1UieVybM3kypqXBfPczaL.jpg",
+        descrição: "Uma adolescente insegura aprende os truques da maquiagem e começa a fazer um enorme sucesso na escola. Mas não vai ser fácil manter as aparências.",
     },
 
     {
-        título: "Como mágica",
-        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/7FBnCkYzHlVHEtuzTEiGgFhs8EQ.jpg",
+        título: "Jogada de Vitória",
+        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/ilYn5vYyhigN6wmVBs7dH2TJ7LS.jpg",
         descrição: "Uma pequena criatura da floresta e um pássaro majestoso trocam de corpo e precisam se unir para sobreviver à aventura mais incrível de suas vidas.",
     },
 
     {
-        título: "Trocando as bolas",
-        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/tYyHmUzo6GT7qjG7OWqo0zsXcoJ.jpg",
-        descrição: "O que pode acontecer quando um magnata de Wall Street encontra um esperto mendigo de rua? Esta é a história de um malandro que troca seu estilo de vida com a de um investidor grã-fino. Daí em diante, a diversão fica mais rica a cada minuto!",
+        título: "O amor mora ao lado",
+        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/lHSnzui4skTidHkAwQ2Qaccdokr.jpg",
+        descrição: "Na tentativa de recomeçar a vida, uma mulher retorna à Coreia e se envolve com alguém do passado. O problema é que existe uma história complicada entre os dois.",
     },
 
     {
-        título: "Coração Partido",
-        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/5YGxIzMxRdcTt4QtjlbV2wYRNlx.jpg",
-        descrição: "Polina, uma estudante do ensino médio, é salva do bullying em sua nova escola e faz um acordo com o principal valentão, Bars: ele deve fingir ser seu namorado e protegê-la, e ela deve fazer tudo o que ele mandar. Durante esse jogo, o casal desenvolve sentimentos verdadeiros, mas sua família e colegas de classe têm motivos para separá-los."
-}]
+        título: "Cães de Caça",
+        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/uFNHrunRIlyzRlRS7c9InUVgFDa.jpg",
+        descrição: "Dois jovens boxeadores unem forças para enfrentar inimigos impiedosos com sede de sangue, arriscando as próprias vidas para fazer justiça e proteger as pessoas que amam.",
+    }
+]
 
-const listaFilmes = document.getElementById("listaFilmes");
-function mostrarFilmes(lista) {
+
+const listaDorama = document.getElementById("listaDorama");
+function mostrarDorama(lista) {
     listaFilmes.innerHTML = ""
-    lista.forEach(filme => {
+    lista.forEach(dorama => {
         listaFilmes.innerHTML += `
             <div class="col-md-4">
-                <div class="card card-filme h-100">
-                    <img src="${filme.imagem}" class="card-img-top">
+                <div class="card card-dorama h-100">
+                    <img src="${dorama.imagem}" class="card-img-top">
                     <div class="card-body">
-                        <h3>${filme.título}</h3>
-                        <p>${filme.descrição}</p>
-                        <button class="btn btn-dark" onclick="verDetalhes('${filme.titulo}')">Ver detalhes</button>
+                        <h3>${dorama.título}</h3>
+                        <p>${dorama.descrição}</p>
+                        <button class="btn btn-dark" onclick="verDetalhes('${dorama.titulo}')">Ver detalhes</button>
                     </div>
                 </div>
             </div>    
@@ -54,12 +56,12 @@ function mostrarFilmes(lista) {
     })
 }
 
-mostrarFilmes(filmes)
+mostrarDorama(dorama)
 
 function verDetalhes(titulo) {
     Swal.fire({
         title: título,
-        text: "Mais informaçaões de Filme",
+        text: "Mais informaçaões de Dorama",
         icon: "info"
     })
 }
